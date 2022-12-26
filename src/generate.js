f400ad6262b39fe0bfb683830f9418bc8872007c
Brayden Langley <brayden167@gmail.com>
2022-12-26 15:57:41 -0800

    Added support for hex and base64 cryptoKeys, and switched to webpack

--- src/generate.js
+++ src/generate.js
@@ -1,8 +1,9 @@
 /**
  * Generates a secure AES-GCM 256 bit key for crypographic operations
- * @returns the generated key in the form of a buffer
+ * @param format the format the key should be returned as (defaults to buffer). Supports base64, hex, and buffer data types.
+ * @returns the generated key
  */
-const generate = async () => {
+const generate = async (format = 'buffer') => {
   return await crypto.subtle.generateKey(
     {
       name: 'AES-GCM',
@@ -12,7 +13,15 @@ const generate = async () => {
     ['encrypt', 'decrypt']
   ).then(async key => {
     const keyBuffer = await crypto.subtle.exportKey('raw', key)
-    return keyBuffer
+    if (format === 'buffer') {
+      return keyBuffer
+    } else if (format === 'base64') {
+      return Buffer.from(keyBuffer).toString('base64')
+    } else if (format === 'hex') {
+      return Buffer.from(keyBuffer).toString('hex')
+    } else {
+      throw TypeError('Specified format is not supported!')
+    }
   })
 }
 export default generate
